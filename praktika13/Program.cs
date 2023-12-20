546611752862c70d269dbdc8ec516f12308bff1b
Artzze <Artzze@FOXPC>
2023-12-20 11:17:48 +0300

    Add project files.

---
+++ praktika13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "alphabet.txt"; // Укажите здесь путь к вашему файлу с алфавитом
+
+        try
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                int symbolCode;
+                while ((symbolCode = reader.Read()) != -1) // Считывание файла посимвольно
+                {
+                    char symbol = (char)symbolCode;
+                    if ((symbol >= 'A' && symbol <= 'Z') || (symbol >= 'a' && symbol <= 'z'))
+                    {
+                        Console.WriteLine($"Символ: {symbol}, Код: {symbolCode}");
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Произошла ошибка при чтении файла: {e.Message}");
+        }
+    }
+}
